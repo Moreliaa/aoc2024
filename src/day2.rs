09277fb763855fd56028ba6c2a9a8135f0e4e1ba
Eric E <eth_er@gmx.de>
2024-12-02 19:02:02 +0100

    day2

---
+++ src/day2.rs
@@ -0,0 +1,43 @@
+pub fn run(input: String) {
+    
+    let mut p1 = 0;
+    let mut p2 = 0;
+
+    for line in  input.lines().into_iter() {
+        let values: Vec<i32> = line.split(" ").map(|l| l.parse::<i32>().unwrap()).collect();
+        let r = check(values, false);
+        p1 += r.0;
+        p2 += r.1;
+    }
+   
+    println!("Part 1: {p1}");
+    println!("Part 2: {p2}");
+}
+
+fn check(values: Vec<i32>, failed_once: bool) -> (i32, i32) {
+    let is_asc = values[0] - values[1] < 0;
+    for i in 0..values.len() - 1 {
+        let v0 = values[i];
+        let v1 = values[i + 1];
+        let diff = v0 - v1;
+        if (is_asc && diff >= 0) || (!is_asc && diff <= 0) || diff.abs() > 3 {
+            if failed_once {
+                return (0, 0);
+            }
+
+            let mut result = (0, 0);
+            for idx in [i, i + 1, 0] {
+                let mut splice1 = values.clone();
+                splice1.splice(idx..idx + 1, []);
+                result = check(splice1, true);
+
+                if result.1 == 1 {
+                    break;
+                }
+            }
+            return (result.0 as i32, result.1 as i32);
+        }
+    }
+    let p1 = if !failed_once { 1 } else { 0 };
+    return (p1, 1);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 #[allow(dead_code)]
 mod day1;
+mod day2;
 extern crate aoc_lib;
 
 fn main() {
-    let year = "2024";
-    let day = "1";
-    let input = aoc_lib::input_reader::get_input(year, day, "cookie.txt");
-    day1::run(input);
+    //day1::run(aoc_lib::input_reader::get_input("2024", "1", "cookie.txt"));
+    day2::run(aoc_lib::input_reader::get_input("2024", "2", "cookie.txt"));
 }
