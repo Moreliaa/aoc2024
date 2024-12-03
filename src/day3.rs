8d1264c24d67d889eb8094b0bd9d0d7128045d13
Eric E <eth_er@gmx.de>
2024-12-03 20:03:41 +0100

    day3

---
+++ src/day3.rs
@@ -0,0 +1,32 @@
+use fancy_regex::Regex;
+
+pub fn run(input: String) {
+    
+    let mut p1 = 0;
+    let mut p2 = 0;
+    let rx = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+        
+    let split = input.split(r"do()");
+    for s in split {
+        let mut s_split = s.split(r"don't()").into_iter();
+        let first = s_split.next().unwrap();
+        let all = rx.captures_iter(first);
+        for m in all {
+            let m = m.unwrap();
+            let result = m[1].parse::<i32>().unwrap() * m[2].parse::<i32>().unwrap();
+            p1 += result;
+            p2 += result;
+        }
+
+        while let Some(content) = s_split.next() {
+            let all = rx.captures_iter(content);
+            for m in all {
+                let m = m.unwrap();
+                p1 += m[1].parse::<i32>().unwrap() * m[2].parse::<i32>().unwrap();
+            }
+        }
+    }
+   
+    println!("Part 1: {p1}");
+    println!("Part 2: {p2}");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 #[allow(dead_code)]
 mod day1;
+#[allow(dead_code)]
 mod day2;
+#[allow(dead_code)]
+mod day3;
 extern crate aoc_lib;
 
 fn main() {
     //day1::run(aoc_lib::input_reader::get_input("2024", "1", "cookie.txt"));
-    day2::run(aoc_lib::input_reader::get_input("2024", "2", "cookie.txt"));
+    //day2::run(aoc_lib::input_reader::get_input("2024", "2", "cookie.txt"));
+    day3::run(aoc_lib::input_reader::get_input("2024", "3", "cookie.txt"));
 }
